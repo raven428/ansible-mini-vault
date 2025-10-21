eb65668e5eca6c7df84ae2159d33cb09f5c9c407
Dmitry Sukhodoev <raven428@gmail.com>
2025-10-21 18:45:39 +0500

    add molecule tests

---
+++ molecule/default/run-tests.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+set -ueo pipefail
+umask 0022
+MY_BIN="$(readlink -f "$0")"
+MY_PATH="$(dirname "${MY_BIN}")"
+cd "${MY_PATH}/../.."
+export ANSIBLE_CONT_ADDONS="--publish=8200:8200"
+# shellcheck disable=1091
+source "${MY_PATH}/../prepare.sh"
+sce='default'
+LOG_PATH="/tmp/molecule-$(/usr/bin/env date '+%Y%m%d%H%M%S.%3N')"
+printf "\n\n\nmolecule [create] action\n"
+ANSIBLE_LOG_PATH="${LOG_PATH}-00create" \
+  ansible-docker.sh molecule -v create -s ${sce}
+n=1
+run_group() {
+  local tag="${1}"
+  local last="${tag:-}"
+  local prefix="${last##*-}"
+  [[ -n "${prefix}" ]] && prefix="-${prefix}"
+  if [[ -z "${tag:-}" ]]; then
+    args=" -- --check"
+  else
+    args=" -- -t ${tag} --check"
+  fi
+  printf "\n\n\nmolecule [converge] %s check\n" "${tag:-empty}"
+  # shellcheck disable=2086
+  ANSIBLE_LOG_PATH="${LOG_PATH}-$(printf %02d $n)converge${prefix}-check" \
+    ansible-docker.sh molecule -v converge -s "${sce}"${args}
+  ((n++))
+  for mode in action check; do
+    args=''
+    if [[ "${mode}" == 'check' ]]; then
+      if [[ -z "${tag:-}" ]]; then
+        args=" -- --check"
+      else
+        args=" -- -t ${tag} --check"
+      fi
+    elif [[ -n "${tag:-}" ]]; then
+      args=" -- -t $tag"
+    fi
+    for stage in converge idempotence; do
+      printf "\n\n\nmolecule [%s] %s %s\n" "${stage}" "${mode}" "${tag}"
+      # shellcheck disable=2086
+      ANSIBLE_LOG_PATH="${LOG_PATH}-$(printf %02d $n)${stage}${prefix}-${mode}" \
+      ANSIBLE_MOLECULE_MODE="${mode}" ANSIBLE_MOLECULE_STAGE="${stage}" \
+        ansible-docker.sh molecule -v "${stage}" -s "${sce}"${args}
+      ((n++))
+    done
+  done
+}
+if [[ "${ANSIBLE_CALL_MODE:-empty}" == 'def1' ]]; then
+  run_group ''
+  export ANSIBLE_PROP_MODE='unseal'
+  run_group 'service-vault,vault-opera'
+  printf "\n\n\nmolecule [converge] unseal fail-variable\n"
+  ANSIBLE_PROP_MODE='fail-variable' \
+    ANSIBLE_LOG_PATH="${LOG_PATH}-$(printf '%02d' "$n")fail-variable" \
+    ansible-docker.sh molecule -v converge -s "${sce}" -- -t service-vault,vault-opera
+  ((n++))
+  printf "\n\n\nmolecule [converge] unseal fail-files\n"
+  ANSIBLE_PROP_MODE='fail-files' \
+    ANSIBLE_LOG_PATH="${LOG_PATH}-$(printf '%02d' "$n")fail-files" \
+    ansible-docker.sh molecule -v converge -s "${sce}" -- -t service-vault,vault-opera
+  ((n++))
+  export ANSIBLE_PROP_MODE='none'
+  run_group 'service-vault,service-stop'
+  run_group 'service-vault,service-destroy'
+elif [[ "${ANSIBLE_CALL_MODE:-empty}" == 'def2' ]]; then
+  run_group 'service-vault,service-start,all'
+  printf "\n\n\nmolecule [converge] service-destroy fail\n"
+  ANSIBLE_PROP_MODE='fail-files' \
+    ANSIBLE_LOG_PATH="${LOG_PATH}-$(printf '%02d' "$n")converge-destroy" \
+    ansible-docker.sh molecule -v converge -s "${sce}" -- -t service-vault,service-destroy
+  ((n++))
+  run_group 'service-vault,service-reset' # reset = stop + destroy
+  export ANSIBLE_PROP_MODE='fail-stop'
+  run_group "service-vault,service-stop"
+else
+  echo "unknown [${ANSIBLE_CALL_MODE:-empty}] in ANSIBLE_CALL_MODE env"
+  exit 1
+fi
---
+++ molecule/prepare.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+set -ueo pipefail
+umask 0022
+: "${CONT_NAME:="ans2dkr-${USER}"}"
+: "${IMAGE_NAME:="ansible-11:latest"}"
+: "${SSH_AUTH_SOCK:="/dev/null"}"
+: "${CONTENGI:="podman"}"
+export DEBIAN_FRONTEND=noninteractive
+/usr/bin/env which sponge >/dev/null || {
+  /usr/bin/env sudo apt-get update &&
+    /usr/bin/env sudo su -c 'DEBIAN_FRONTEND=noninteractive apt-get install -y moreutils'
+}
+/usr/bin/env which ansible-docker.sh >/dev/null || {
+  /usr/bin/env sudo curl -fsSLm 11 -o /usr/local/bin/ansible-docker.sh \
+    https://raw.githubusercontent.com/raven428/container-images/refs/heads/master/_shared/install/ansible/ansible-docker.sh
+  /usr/bin/env sudo chmod 755 /usr/local/bin/ansible-docker.sh
+  # remove after replace docker to podman inside carrier
+  /usr/bin/env sudo sed -i 's/--network=host//g' /usr/local/bin/ansible-docker.sh
+}
+[[ -v GITHUB_JOB ]] && /usr/bin/env curl -fsSLm 11 \
+  https://raw.githubusercontent.com/raven428/container-images/refs/heads/master/podman.sh | /usr/bin/env sudo bash
+[[ "${SSH_AUTH_SOCK}" == "/dev/null" ]] && export SSH_AUTH_SOCK
+[[ -v SKIP_DID ]] || {
+  export ANSIBLE_CONT_COMMAND=' '
+  IMAGE_NAME="docker-${IMAGE_NAME}"
+}
+mkdir -vp "${HOME}"/.{ansible_async,cache}
+ANSIBLE_CONT_NAME="${CONT_NAME}"
+ANSIBLE_IMAGE_NAME="ghcr.io/raven428/container-images/${IMAGE_NAME}"
+export CONTENGI ANSIBLE_CONT_NAME ANSIBLE_IMAGE_NAME
+CONT_BRIDGE_NAME='bridge'
+[[ "${CONTENGI}" == "podman" ]] && CONT_BRIDGE_NAME='podman'
+{
+  ANSIBLE_CONT_ADDONS=" \
+    ${ANSIBLE_CONT_ADDONS:-} \
+    --network=${CONT_BRIDGE_NAME} \
+    -u 0 --privileged --userns=keep-id \
+    --tmpfs /sys/fs/cgroup:rw,nosuid,noexec,nodev,mode=755 \
+    -v ${HOME}/.cache:${HOME}/.cache:rw \
+    -v ${HOME}/.ansible_async:${HOME}/.ansible_async:rw \
+    --cap-add=NET_ADMIN,SYS_MODULE,SYS_ADMIN --replace \
+  " /usr/bin/env ansible-docker.sh true
+}
+[[ -v SKIP_DID ]] || {
+  count=7
+  while ! /usr/bin/env "${CONTENGI}" exec "${CONT_NAME}" systemctl status docker; do
+    echo "waiting container ready, left [$count] tries"
+    count=$((count - 1))
+    if [[ $count -le 0 ]]; then
+      echo "unable to start docker daemon inside ${CONTENGI} container"
+      exit 1
+    fi
+    sleep 1
+  done
+}
+export ANSIBLE_ROLES_PATH='/tmp/ansible/roles2test'
+deps_dir='deps/roles'
+[[ -d ${deps_dir} ]] || {
+  /usr/bin/env mkdir -vp ${deps_dir}
+  /usr/bin/env rm -vf "${deps_dir}/ansible-mini-vault"
+  /usr/bin/env ln -sfv ../.. "${deps_dir}/ansible-mini-vault"
+}
