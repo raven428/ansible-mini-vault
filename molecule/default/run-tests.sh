f760a2b5f4a3007da95512c4691f6777b8ad7ac7
Dmitry Sukhodoev <raven428@gmail.com>
2026-04-26 13:58:37 +0000

    replace container to appimage

--- molecule/default/run-tests.sh
+++ molecule/default/run-tests.sh
@@ -1,17 +1,45 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 umask 0022
-MY_BIN="$(readlink -f "$0")"
+MY_BIN="$(realpath "$0")"
 MY_PATH="$(dirname "${MY_BIN}")"
 cd "${MY_PATH}/../.."
-export ANSIBLE_CONT_ADDONS="--publish=8200:8200"
 # shellcheck disable=1091
 source "${MY_PATH}/../prepare.sh"
 sce='default'
 LOG_PATH="/tmp/molecule-$(/usr/bin/env date '+%Y%m%d%H%M%S.%3N')"
 printf "\n\n\nmolecule [create] action\n"
-ANSIBLE_LOG_PATH="${LOG_PATH}-00create" \
-  ansible-docker.sh molecule -v create -s ${sce}
+# shellcheck disable=2154
+ANSIBLE_LOG_PATH="${LOG_PATH}-00create" "${_appimage_bin}" molecule -v create -s ${sce}
+# Override galaxy-installed roles with specific commits when requested.
+# Must run after molecule create (which triggers galaxy dependency install).
+if [[ -n "${MEGA_VAR_REF:-}" ]]; then
+  echo "MEGA_VAR_REF=${MEGA_VAR_REF}"
+  /usr/bin/env rm -rf "${ANSIBLE_ROLES_PATH}/raven428.mega_var"
+  /usr/bin/env git clone --branch "${MEGA_VAR_REF}" \
+    'https://github.com/raven428/ansible-mega-var.git' \
+    "${ANSIBLE_ROLES_PATH}/raven428.mega_var"
+else
+  echo 'MEGA_VAR_REF input missed'
+fi
+if [[ -n "${MEGA_LAUNCH_REF:-}" ]]; then
+  echo "MEGA_LAUNCH_REF=${MEGA_LAUNCH_REF}"
+  /usr/bin/env rm -rf "${ANSIBLE_ROLES_PATH}/raven428.mega_launch"
+  /usr/bin/env git clone --branch "${MEGA_LAUNCH_REF}" \
+    'https://github.com/raven428/ansible-mega-launch.git' \
+    "${ANSIBLE_ROLES_PATH}/raven428.mega_launch"
+else
+  echo 'MEGA_LAUNCH_REF input missed'
+fi
+if [[ -n "${MEGA_SERVICE_REF:-}" ]]; then
+  echo "MEGA_SERVICE_REF=${MEGA_SERVICE_REF}"
+  /usr/bin/env rm -rf "${ANSIBLE_ROLES_PATH}/raven428.mega_service"
+  /usr/bin/env git clone --branch "${MEGA_SERVICE_REF}" \
+    'https://github.com/raven428/ansible-mega-service.git' \
+    "${ANSIBLE_ROLES_PATH}/raven428.mega_service"
+else
+  echo 'MEGA_SERVICE_REF input missed'
+fi
 n=1
 run_group() {
   local tag="${1}"
@@ -26,7 +54,7 @@ run_group() {
   printf "\n\n\nmolecule [converge] %s check\n" "${tag:-empty}"
   # shellcheck disable=2086
   ANSIBLE_LOG_PATH="${LOG_PATH}-$(printf %02d $n)converge${prefix}-check" \
-    ansible-docker.sh molecule -v converge -s "${sce}"${args}
+    "${_appimage_bin}" molecule -v converge -s "${sce}"${args}
   ((n++))
   for mode in action check; do
     args=''
@@ -44,7 +72,7 @@ run_group() {
       # shellcheck disable=2086
       ANSIBLE_LOG_PATH="${LOG_PATH}-$(printf %02d $n)${stage}${prefix}-${mode}" \
       ANSIBLE_MOLECULE_MODE="${mode}" ANSIBLE_MOLECULE_STAGE="${stage}" \
-        ansible-docker.sh molecule -v "${stage}" -s "${sce}"${args}
+        "${_appimage_bin}" molecule -v "${stage}" -s "${sce}"${args}
       ((n++))
     done
   done
@@ -56,12 +84,12 @@ if [[ "${ANSIBLE_CALL_MODE:-empty}" == 'def1' ]]; then
   printf "\n\n\nmolecule [converge] unseal fail-variable\n"
   ANSIBLE_PROP_MODE='fail-variable' \
     ANSIBLE_LOG_PATH="${LOG_PATH}-$(printf '%02d' "$n")fail-variable" \
-    ansible-docker.sh molecule -v converge -s "${sce}" -- -t service-vault,vault-opera
+    "${_appimage_bin}" molecule -v converge -s "${sce}" -- -t service-vault,vault-opera
   ((n++))
   printf "\n\n\nmolecule [converge] unseal fail-files\n"
   ANSIBLE_PROP_MODE='fail-files' \
     ANSIBLE_LOG_PATH="${LOG_PATH}-$(printf '%02d' "$n")fail-files" \
-    ansible-docker.sh molecule -v converge -s "${sce}" -- -t service-vault,vault-opera
+    "${_appimage_bin}" molecule -v converge -s "${sce}" -- -t service-vault,vault-opera
   ((n++))
   export ANSIBLE_PROP_MODE='none'
   run_group 'service-vault,service-stop'
@@ -71,13 +99,14 @@ elif [[ "${ANSIBLE_CALL_MODE:-empty}" == 'def2' ]]; then
   printf "\n\n\nmolecule [converge] service-destroy fail\n"
   ANSIBLE_PROP_MODE='fail-files' \
     ANSIBLE_LOG_PATH="${LOG_PATH}-$(printf '%02d' "$n")converge-destroy" \
-    ansible-docker.sh molecule -v converge -s "${sce}" -- -t service-vault,service-destroy
+    "${_appimage_bin}" molecule -v converge -s "${sce}" -- -t \
+    service-vault,service-destroy
   ((n++))
   run_group 'service-vault,service-reset' # reset = stop + destroy
   export ANSIBLE_PROP_MODE='fail-stop'
   run_group "service-vault,service-stop"
 else
   echo "unknown [${ANSIBLE_CALL_MODE:-empty}] in ANSIBLE_CALL_MODE env"
-  ansible-docker.sh molecule -v converge -s "${sce}"
+  "${_appimage_bin}" molecule -v converge -s "${sce}"
   exit 1
 fi
--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 umask 0022
-MY_BIN="$(readlink -f "$0")"
+MY_BIN="$(realpath "$0")"
 MY_PATH="$(dirname "${MY_BIN}")"
 cd "${MY_PATH}/.."
 # shellcheck disable=1091
-SKIP_DID=1 source "${MY_PATH}/../molecule/prepare.sh"
-ansible-docker.sh pytest --color=yes \
+source "${MY_PATH}/../molecule/prepare.sh"
+# shellcheck disable=2154
+"${_appimage_bin}" pytest --color=yes \
   -vv -o cache_dir=/tmp/pytests tests/vault_hcl/test_vault_hcl.py
